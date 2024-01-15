75a753a657126b19e3717c0ffdccd78fbf79697f
Dagim Desta <71279457+dddesta@users.noreply.github.com>
2024-01-15 17:27:05 -0500

    Synchronizing job JSONETL-Transform from AWS Glue at 2024/01/15 22:27:05

--- JSONETL-Transform/JSONETL-Transform.py
+++ JSONETL-Transform/JSONETL-Transform.py
@@ -5,8 +5,8 @@ from pyspark.context import SparkContext
 # sys.argv[0] is the script name(jobName)
 
 s3_bucket=sys.argv[1] if len(sys.argv)>1 else None
-s3_key=sys.argv[2] is len(sys.argv)>2 else None
-s3_path=sys.argv[3] is len(sys.argv)>3 else None
+s3_key=sys.argv[2] if len(sys.argv)>2 else None
+s3_path=sys.argv[3] if len(sys.argv)>3 else None
 
 sc =SparkContext.getOrCreate()
 glueContext=GlueContext(sc)
@@ -20,14 +20,35 @@ dynamicFrame = glueContext.create_dynamic_frame.from_options(
         format='json'
     )
     
-def unnested_json(json_data,parent_key='',sep=''):
+def unnest_json(json_data,parent_key='',sep='_'):
     unnested_data={}
     
     for key,value in json_data.items():
         new_key= f'{parent_key}{sep}{key}' if parent_key else key
-        
-        if isinstance(value,dict):
-            unnested_data.update()
 
+        #if the value is a dictionary
+        if isinstance(value,dict):
+            unnested_data.update(unnest_json(value,new_key,sep=sep))
+        
+        elif isinstance(value,list):
+            for i,item in enumerate(value):
+                unnested_data.update(unnest_json({str(i)},new_key,sep=sep))
+        #leaf value
+        else:
+            unnested_data[new_key]=value
+    
+    return unnested_data
+
+reslut_df=unnest_json(dynamicFrame)
+
+glueContext.write_dynamic_fram.from_options(
+    frame=dynamicFrame,
+    connection_type='s3',
+    connection_options={
+       s3://etl-final-destination/JSON Final/
+    },
+    format='parquet',
+    format_options={'snappy'}
+    )
 
 
