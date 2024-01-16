f766a517755a7dea99eb620a818e02021a881eae
Dagim Desta <71279457+dddesta@users.noreply.github.com>
2024-01-16 18:42:41 -0500

    Synchronizing job JSONETL-Transform from AWS Glue at 2024/01/16 23:42:40

--- JSONETL-Transform/JSONETL-Transform.py
+++ JSONETL-Transform/JSONETL-Transform.py
@@ -1,6 +1,6 @@
 import sys
-from awsglue.context import GlueContext
-from pyspark.context import SparkContext
+import boto3
+import awswrangler as wr
 
 # sys.argv[0] is the script name(jobName)
 
@@ -8,17 +8,11 @@ s3_bucket=sys.argv[1] if len(sys.argv)>1 else None
 s3_key=sys.argv[2] if len(sys.argv)>2 else None
 s3_path=sys.argv[3] if len(sys.argv)>3 else None
 
-sc =SparkContext.getOrCreate()
-glueContext=GlueContext(sc)
-spark=glueContext.spark_session
 
 # for testing purposes: Manually lasgebaw
 
-dynamicFrame = glueContext.create_dynamic_frame.from_options(
-        connection_type='s3',
-        connection_options={'paths':[s3_path]},
-        format='json'
-    )
+df=wr.s3.read_json([s3_path])
+
     
 def unnest_json(json_data,parent_key='',sep='_'):
     unnested_data={}
@@ -32,23 +26,20 @@ def unnest_json(json_data,parent_key='',sep='_'):
         
         elif isinstance(value,list):
             for i,item in enumerate(value):
-                unnested_data.update(unnest_json({str(i)},new_key,sep=sep))
+                unnested_data.update(unnest_json({str(i):item},new_key,sep=sep))
         #leaf value
         else:
             unnested_data[new_key]=value
     
     return unnested_data
 
-reslut_df=unnest_json(dynamicFrame)
 
-glueContext.write_dynamic_fram.from_options(
-    frame=dynamicFrame,
-    connection_type='s3',
-    connection_options={
-       s3://etl-final-destination/JSON Final/
-    },
-    format='parquet',
-    format_options={'snappy'}
-    )
+result_df=unnest_json(df.toDF().toJSON().first().asDict())
+
+destination_bucket = 'etl-final-destination'
+destination_key = 'final.snappy.parquet'
+dest_path=f's3://{destination_bucket}/{destination_key}'
+
+wr.s3.to_parquet(result_df,dest_path)
 
 
