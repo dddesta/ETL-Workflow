69669ea6b50d8451589fc6334edd7381986158b6
Dagim Desta <71279457+dddesta@users.noreply.github.com>
2024-02-08 22:23:17 -0500

    Synchronizing job ETL-Transform from AWS Glue at 2024/02/09 03:23:17

--- ETL-Transform/ETL-Transform.py
+++ ETL-Transform/ETL-Transform.py
@@ -1,58 +1,67 @@
+import awswrangler as wr
 import boto3
 import pandas as pd
-import awswrangler as wr
 
-def
+#send sns to send notifications to an sns topic. for success and failure
+def send_sns(bucket,key,status=False,e=''):
+    sns=boto3.client('sns')
+    sub='CSV Transform Update!'
+    
+    if status:
+        mssg=f'SUCCESS! The file {key} in {bucket} has been processed successfully'
+    else:
+        mssg=f'FAILURE! The file {key} in {bucket} has not been processed. \n \n Exception: {e} '
+        
+    sns.publish(
+        TopicArn='arn:aws:sns:us-east-1:385363378908:ETLWorkFlowTopic',
+        Message=mssg,
+        Subject=sub
+        )
+    
+# main function to extract load and transform the data
+def main_func():
+    try:
+        input_bucket='s3-etlproject-drop'
+        input_key='data.csv'
+        input_path='s3://s3-etlproject-drop/data.csv'
+        
+        output_path='s3://etl-final-destination/csvout.parquet'
+    
+        #read csv from s3 using wrangler
+        df=wr.s3.read_csv(input_path)
+        
+        #data dictionary ro transform data
+        data_dict= {'SITE_CM360':str, 'SITE_ID':str,
+            'SITE_ID_SITE_DIRECTORY':'int',
+            'SITE_SITE_DIRECTORY':str,
+            'FILENAME':str,
+            'FILE_ROW_NUMBER':'int'}
+        
+        #transform using pandas
+        df.astype(data_dict)
+        
+        # change these columns to datetime
+        df['PARTITION_DATE']= pd.to_datetime(df['PARTITION_DATE'])
+        df['INSERT_DATETIME']= pd.to_datetime(df['INSERT_DATETIME'])
+        
+        
+        # success notif
+        send_sns(input_bucket,input_key,True)
 
-input_bucket_name = 's3-etlproject-drop'
-input_object_key = 'data.csv'
-output_bucket_name = 'etl-final-destination'
-output_object_key = 'outputdata.parquet'
+        # save as parquet
+        wr.s3.to_parquet(df, output_path,compression='snappy')
+    
+    except Exception as e:
+        # failure notif
+        send_sns(input_bucket,input_key,False,str(e))
+        
 
-data_dictionary = {
-    'SITE_CM360': 'string',
-    'SITE_ID': 'string',
-    'SITE_ID_SITE_DIRECTORY': 'int',
-    'SITE_SITE_DIRECTORY': 'string',
-    'FILENAME': 'string',
-    'FILE_ROW_NUMBER': 'int',
-    'INSERT_DATETIME': 'timestamp',
-    'PARTITION_DATE': 'date'
-}
-
-#reading the csv and putting it on the pandas df
-s3 = boto3.client('s3')
-
-response = s3.get_object(Bucket=input_bucket_name, Key=input_object_key)
-csv_content = response['Body'].read().decode('utf-8')
-
-df = pd.read_csv(StringIO(csv_content))
-df['INSERT_DATETIME'] = pd.to_datetime(df['INSERT_DATETIME'])
-
-df['SITE_ID'] = df['SITE_ID'].astype(str)
-df['PARTITION_DATE'] = pd.to_datetime(df['PARTITION_DATE']).dt.date
-
-#Then translate the data dict into appropriate pyarrow names
-# Convert the DataFrame to a PyArrow Table with enforced data types
-table_schema = pa.schema([(col, pa.int64()) if data_type == 'int' else
-                          (col, pa.timestamp('us')) if data_type == 'timestamp' else
-                          (col, pa.date32()) if data_type == 'date' else
-                          (col, pa.string())
-                          for col, data_type in data_dictionary.items()])
-
-#Then convert the DataFrame into a pyarrowtable
-table = pa.Table.from_pandas(df, schema=table_schema)
-
-
-# Write the PyArrow Table to a BytesIO object with Snappy compression
-buffer = BytesIO()
-pq.write_table(table, buffer, compression='snappy')
-
-# Specify the destination S3 bucket and key for the Parquet file
-destination_bucket = 'etl-final-destination'
-destination_key = 'final.snappy.parquet'
-
-
-# Upload the BytesIO buffer to S3
-s3.put_object(Body=buffer.getvalue(), Bucket=output_bucket_name, Key=output_object_key)
-
+def lambda_handler(event, context):
+    # TODO implement
+    main_func()
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+    
